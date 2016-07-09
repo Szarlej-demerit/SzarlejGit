d0e0673b7876dbb6693b53aee6f0e4fcd91bf08a
Szarlej-demerit <szarlej.demerit@gmail.com>
2016-07-10 02:41:47 +0300

    LCD

---
+++ Test/IAR ARM - Debug/display.d
@@ -0,0 +1,23 @@
+lcd-graphics/display.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\submodules\kits\common\drivers\display.c
+lcd-graphics/display.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\stdint.h
+lcd-graphics/display.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\ycheck.h
+lcd-graphics/display.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\yvals.h
+lcd-graphics/display.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\DLib_Defaults.h
+lcd-graphics/display.o: D:\Soft\IAR\ Systems\Embedded\ Workbench\ 7.3\arm\inc\c\DLib_Config_Normal.h
+lcd-graphics/display.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\DLib_Product.h
+lcd-graphics/display.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\xencoding_limits.h
+lcd-graphics/display.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\DLib_Threads.h
+lcd-graphics/display.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\stdbool.h
+lcd-graphics/display.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\string.h
+lcd-graphics/display.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\ysizet.h
+lcd-graphics/display.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\DLib_Product_string.h
+lcd-graphics/display.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\submodules\kits\common\drivers\displayconfigall.h
+lcd-graphics/display.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\submodules\kits\EFR32MG1_BRD4151A\config\displaypalconfig.h
+lcd-graphics/display.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\submodules\kits\EFR32MG1_BRD4151A\config\displayconfig.h
+lcd-graphics/display.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\utils\graphics\displayconfigapp.h
+lcd-graphics/display.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\submodules\kits\EFR32MG1_BRD4151A\config\displayls013b7dh03config.h
+lcd-graphics/display.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\submodules\kits\common\drivers\displayls013b7dh03.h
+lcd-graphics/display.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\submodules\kits\common\drivers\emstatus.h
+lcd-graphics/display.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\submodules\reptile\glib\em_types.h
+lcd-graphics/display.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\submodules\kits\common\drivers\display.h
+lcd-graphics/display.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\submodules\kits\common\drivers\displaybackend.h
---
+++ Test/IAR ARM - Debug/response_print.d
@@ -0,0 +1,18 @@
+response-printf/response_print.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\utils\response_print\response_print.c
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\stdio.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\ycheck.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\yvals.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\DLib_Defaults.h
+response-printf/response_print.o: D:\Soft\IAR\ Systems\Embedded\ Workbench\ 7.3\arm\inc\c\DLib_Config_Normal.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\DLib_Product.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\xencoding_limits.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\DLib_Threads.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\ysizet.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\ystdio.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\stdlib.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\stdint.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\string.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\DLib_Product_string.h
+response-printf/response_print.o: D:\Projects\Gecko\SiliconLabs\SiliconLabsRAIL\1.2.0.0-GA\utils\response_print\response_print.h
+response-printf/response_print.o: d:\Soft\IARSYS~1\EMBEDD~1.3\arm\inc\c\stdbool.h
+response-printf/response_print.o: C:\Users\Szarlej\SimplicityStudio\v3_workspace\Test\rail-configuration.h
